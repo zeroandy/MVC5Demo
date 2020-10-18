be9d0adeba63b17fff53c49d9a89aef163895f98
AndyLiu <zero.andy@msa.hinet.net>
2020-10-18 15:24:37 +0800

    update for Required and Range error message

--- Models/Person.cs
+++ Models/Person.cs
@@ -11,8 +11,8 @@ namespace MVC5Demo.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "請輸入姓名")]
         public string Name { get; set; }
-        [Required(ErrorMessage ="請輸入18 ~ 99")]
-        [Range(18, 99)]
+        [Required(ErrorMessage ="請輸入年紀")]
+        [Range(18, 99, ErrorMessage ="請輸入{1} ~ {2}")]
         public int Age { get; set; }
     }
 }
