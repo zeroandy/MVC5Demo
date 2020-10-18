f61bf9aefb0974a76c06b390b6d781cf8183336b
AndyLiu <zero.andy@msa.hinet.net>
2020-10-18 15:38:02 +0800

    多國語系驗證寫法

--- Models/Person.cs
+++ Models/Person.cs
@@ -9,7 +9,7 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "請輸入姓名")]
+        [Required(ErrorMessageResourceName ="Person_Name_req", ErrorMessageResourceType = typeof(Resource1))]
         public string Name { get; set; }
         [Required(ErrorMessage ="請輸入年紀")]
         [Range(18, 99, ErrorMessage ="請輸入{1} ~ {2}")]
