2205bbb101dba3423e5de7396809e38837be866f
AndyLiu <zero.andy@msa.hinet.net>
2020-10-18 17:26:46 +0800

    10 加入 Courses 控制器（使用 ADO.NET 實體資料模型自動產生程式碼）

---
+++ Models/MyPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Demo.Models
+{
+    public class MyPerson
+    {
+        public int Id { get; set; }
+        //[DisplayName("姓名")]
+        [Display(Name = "Col_Name1", ResourceType = typeof(Resource1))]
+        [Required(ErrorMessageResourceName = "Person_Name_req", ErrorMessageResourceType = typeof(Resource1))]
+        public string Name { get; set; }
+        //[DisplayName("年齡")]
+        [Display(Name = "Col_Age", ResourceType = typeof(Resource1))]
+        [Required(ErrorMessageResourceName = "Person_Age_req", ErrorMessageResourceType = typeof(Resource1))]
+        [Range(18, 99, ErrorMessageResourceName = "Person_Age_ran", ErrorMessageResourceType = typeof(Resource1))]
+        public int Age { get; set; }
+    }
+}
