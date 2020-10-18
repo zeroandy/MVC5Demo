bf104cd80cb767610b47a0c3eb2b7d012ab4003c
AndyLiu <zero.andy@msa.hinet.net>
2020-10-18 10:54:30 +0800

    04 加入 Person 模型類別與 TestController 與建立 Index 檢視頁面

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Demo.Models;
+
+namespace MVC5Demo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var data = new List<Person>()
+            {
+                new Person() {Id = 1, Name = "A", Age = 18},
+                new Person() {Id = 2, Name = "B", Age = 19},
+                new Person() {Id = 3, Name = "C", Age = 20},
+                new Person() {Id = 4, Name = "D", Age = 21},
+
+            };
+
+
+            return View(data);
+        }
+    }
+}
