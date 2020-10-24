8cef883408fdb6d70afb8fc2a21f689f2ed66b7c
AndyLiu <zero.andy@msa.hinet.net>
2020-10-24 11:00:05 +0800

    11 加入 Departments 控制器並利用 ContosoUniversityEntities 操作資料庫

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,120 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private ContosoUniversityEntities db = new ContosoUniversityEntities();
+
+        // GET: Departments
+        public ActionResult Index()
+        {
+
+            return View(db.Department.ToList() );
+        }
+
+        public ActionResult Details(int? id)
+        {
+
+            return View(db.Department.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Department.Add(department);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+
+            return View(department);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return this.HttpNotFound();
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+
+            return View(db.Department.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Department.Find(id);
+
+                item.Budget = department.Budget;
+                item.Name = department.Name;
+                item.StartDate = department.StartDate;
+                item.InstructorID = department.InstructorID;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(db.Department.Find(id));
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var item = db.Department.Find(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Department.Find(id);
+
+                if (item == null)
+                {
+                    return HttpNotFound();
+                }
+
+                db.Department.Remove(item);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
