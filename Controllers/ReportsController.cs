286b6e8947705eea23d14d0e80c7b0ca83216096
AndyLiu <zero.andy@msa.hinet.net>
2020-10-25 10:09:25 +0800

    17 新增 ReportsController 並撰寫三種不同的報表頁面實作

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Demo.Models;
+using MVC5Demo.Models.VIewModel;
+
+namespace MVC5Demo.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private ContosoUniversityEntities db = new ContosoUniversityEntities();
+
+        public ReportsController()
+        {
+            db.Database.Log = (msg) =>
+            {
+                Debug.WriteLine("----------------------");
+                Debug.WriteLine(msg);
+                Debug.WriteLine("----------------------");
+            };
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CoursesReport1()
+        {
+            var data = (from c in db.Course
+                select new CoursesReport1VM()
+                {
+                    CourseId = c.CourseID,
+                    CourseName = c.Title,
+                    StudentCount = c.Enrollments.Count(),
+                    TeacherCount = c.Teachers.Count(),
+                    AvgGrade = c.Enrollments.Where(e => e.Grade.HasValue).Average(e => e.Grade).Value
+                }).ToList();
+            return View(data);
+        }
+
+        public ActionResult CoursesReport2()
+        {
+            var data = db.Database.SqlQuery<CoursesReport1VM>(@"select Course.CourseID,Course.Title as CourseName,
+(select COUNT(CourseID) from CourseInstructor where (CourseID = Course.CourseID)) as TeacherCount,
+(select COUNT(CourseID) from Enrollment where (Course.CourseID = Enrollment.CourseID)) as StudentCount,
+(select AVG(CAST(Grade as Float)) from Enrollment where (Course.CourseID = Enrollment.CourseID)) as AvgGrade
+from Course
+Group by Course.CourseID,course.Title");
+            return View("CoursesReport1", data);
+        }
+
+        public ActionResult CoursesReport3(int id)
+        {
+            var data = db.Database.SqlQuery<CoursesReport1VM>(@"select Course.CourseID,Course.Title as CourseName,
+(select COUNT(CourseID) from CourseInstructor where (CourseID = Course.CourseID)) as TeacherCount,
+(select COUNT(CourseID) from Enrollment where (Course.CourseID = Enrollment.CourseID)) as StudentCount,
+(select AVG(CAST(Grade as Float)) from Enrollment where (Course.CourseID = Enrollment.CourseID)) as AvgGrade
+from Course
+where Course.CourseID = @p0
+Group by Course.CourseID,course.Title", id);
+            return View("CoursesReport1", data);
+        }
+    }
+}
---
+++ Models/VIewModel/CoursesReport1VM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Demo.Models.VIewModel
+{
+    public class CoursesReport1VM
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int TeacherCount { get; set; }
+        public int StudentCount { get; set; }
+        public double? AvgGrade { get; set; }
+    }
+}
