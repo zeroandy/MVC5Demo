2ce2d4193a426d277449af2607e22cdecefc3ebf
AndyLiu <zero.andy@msa.hinet.net>
2020-11-01 16:35:08 +0800

    @Html.DisplayForModel() 的用法

--- Models/VIewModel/LoginVM.cs
+++ Models/VIewModel/LoginVM.cs
@@ -13,6 +13,7 @@ namespace MVC5Demo.Models.VIewModel
 
         [Required]
         [MinLength(3), MaxLength(16)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
